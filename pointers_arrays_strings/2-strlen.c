33d58d19b907e39076e93c8195eb3424f31b5540
Yahaya <azconcept2016@gmail.com>
2022-10-20 22:55:33 +0100

    feat: find length of string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int _strlen(char *s);
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
+
+/**
+ * _strlen - finds the length
+ * @s: pointer to string
+ * Return: the length of a string.
+ */
+int _strlen(char *s)
+{
+    int i = 0;
+
+    while (*(s + i))
+    {
+        i++;
+    }
+
+    return (i);
+}
