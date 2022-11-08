12c43e0fd3504553453badf3910fb85948172d2f
Yahaya <azconcept2016@gmail.com>
2022-11-08 17:58:50 +0100

    :tada: feat: add custom atoi function

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+int _atoi(char *s);
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int nb;
+
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	nb = _atoi("-402");
+	printf("%d\n", nb);
+	nb = _atoi("214748364");
+	printf("%d\n", nb);
+	nb = _atoi("0");
+	printf("%d\n", nb);
+	nb = _atoi("azeez");
+	printf("%d\n", nb);
+	nb = _atoi("Suite 402");
+	printf("%d\n", nb);
+	/*nb = _atoi(" ------++++++-----+++++--98");
+	printf("%d\n", nb);
+	nb = _atoi(" + + - -98 Battery Street; San Francisco, CA 94111 - USA ");
+	printf("%d\n", nb);
+	nb = _atoi("---++++ -++ Sui - te - 402 #cisfun :)");
+	printf("%d\n", nb);*/
+	return (0);
+}
+
+int _atoi(char *s)
+{
+	int int_value = 0, sign = 1;
+
+	/*If the first character is minus*/
+	if (*s == '-')
+	{
+		sign = -1;
+		s++;
+	}
+
+	/*Convert string number to integer*/
+	while (*s)
+	{
+		/*If the string is a mixture of numbers*/
+		/*and letters extract the number*/
+		if (*s >= '0' && *s <= '9')
+		{
+			int_value = (int_value * 10) + (*s - '0');
+		}
+		s++;
+	}
+	return (int_value * sign);
+}
