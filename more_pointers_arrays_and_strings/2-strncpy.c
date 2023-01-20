8632e8dd239bc38aea7b4d7b1e6a7c57df4f9fa1
azconcept-droid <azconcept2016@gmail.com>
2023-01-20 01:25:17 +0100

    create folder for strncpy main.c test functions

--- more_pointers_arrays_and_strings/2-strncpy.c
+++ more_pointers_arrays_and_strings/2-strncpy.c
@@ -1,36 +1,44 @@
 #include <stdio.h>
-
-char *_strncpy(char *, char *, int);
-
+char *_strncpy(char *dest, char *src, int n);
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char s1[98];
-	char *p;
-	int i;
+    char s1[98];
+    char *ptr;
+    int i;
 
-	for (i = 0; i < 98 - 1; i++)
-		s1[i] = '-';
-	s1[i] = '\0';
-	printf("%s\n", s1);
-	p = _strncpy(s1, "Talk is cheap. Show me the code.\n", 14);
-	printf("%s\n", s1);
-	printf("%s\n", p);
-	for (i = 0; i < 98; i++)
-	{
-		if (i % 10)
-			printf(" ");
-		if (!(i % 10) && i)
-			printf("\n");
-		printf("0x%02x", s1[i]);
-	}
-	printf("\n");
-	return (0);
+    for (i = 0; i < 98 - 1; i++)
+    {
+        s1[i] = '*';
+    }
+    s1[i] = '\0';
+    printf("%s\n", s1);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+    printf("%s\n", s1);
+    printf("%s\n", ptr);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+    printf("%s", s1);
+    printf("%s", ptr);
+    for (i = 0; i < 98; i++)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", s1[i]);
+    }
+    printf("\n");
+    return (0);
 }
+
 int _strlen(char *str)
 {
 	int count = 0;
@@ -48,7 +56,6 @@ char *_strncpy(char *dest, char *src, int n)
 	int i, j = 0, k;
 
 	i = _strlen(src);
-
 	if (n <= i)
 	{
 		for (k = 0; k < n; k++, j++)
