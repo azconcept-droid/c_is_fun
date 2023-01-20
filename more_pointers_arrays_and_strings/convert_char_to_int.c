990322b78fb6cd8b6d70ce9a16c78b979e784aad
azconcept-droid <azconcept2016@gmail.com>
2023-01-20 02:19:49 +0100

    Convert character to integer and integer back to character

---
+++ more_pointers_arrays_and_strings/convert_char_to_int.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char one_in_string = '1', two_in_string = '2';
+    int one_in_num = one_in_string - '0';
+    int two_in_num = two_in_string - '0';
+
+    printf("\'1\' in integer is %d\n", one_in_num);
+    printf("\'2\' in integer is %d\n", two_in_num);
+    printf("Convert back to integer\n");
+    printf("1 in character is \'%c\'\n", one_in_num + '0');
+    printf("2 in character is \'%c\'\n", two_in_num + '0');
+
+    return (0);
+}
