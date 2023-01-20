be7f0debd8406dfacb1b8e199cf943ded8885a1a
azconcept-droid <azconcept2016@gmail.com>
2023-01-20 12:02:34 +0100

    :tada: update betty doc and code style

--- more_pointers_arrays_and_strings/3-strcmp.c
+++ more_pointers_arrays_and_strings/3-strcmp.c
@@ -8,24 +8,29 @@
  */
 int main(void)
 {
-	char s1[] = "Hello";
-	char s2[] = "World!";
+	char s1[] = "Holberton";
+	char s2[] = "Holbeerton";
 
-	/*printf("%p\n", s1);
-	printf("%p\n", s2);*/
 	printf("%d\n", _strcmp(s1, s2));
 	printf("%d\n", _strcmp(s2, s1));
 	printf("%d\n", _strcmp(s1, s1));
 	return (0);
 }
-
+/**
+ * _strcmp - compare two strings
+ * @s1 : first string
+ * @s2 : second string
+ *
+ * Return: the compared result
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	/*THOUGTH PROCESS
-	1. Run through s1[i] & s2[i]
-	2. IF s1[i] - s2[i] != 0 RETURN (s1[i] - s2[i])
-	3. RETURN 0.
+	/**
+	* THOUGTH PROCESS
+	* 1. Run through s1[i] & s2[i]
+	* 2. IF s1[i] - s2[i] != 0 RETURN (s1[i] - s2[i])
+	* 3. RETURN 0.
 	*/
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
